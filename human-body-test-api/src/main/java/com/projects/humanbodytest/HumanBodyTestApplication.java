c30c2a8ea280f24c8703e7586ed88a42816e5bb4
VladmirVolgen <alvaroalonsoflor@gmail.com>
2019-01-12 20:31:00 +0000

    App now reading excel file and storing into a var

--- human-body-test-api/src/main/java/com/projects/humanbodytest/HumanBodyTestApplication.java
+++ human-body-test-api/src/main/java/com/projects/humanbodytest/HumanBodyTestApplication.java
@@ -1,12 +1,25 @@
 package com.projects.humanbodytest;
 
+import java.io.IOException;
+import java.util.ArrayList;
+
+import org.apache.poi.EncryptedDocumentException;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import com.projects.humanbodytest.models.ExcelReader;
 
 @SpringBootApplication
 public class HumanBodyTestApplication {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws EncryptedDocumentException, IOException {
+		
+		ExcelReader excelReader = new ExcelReader();
+		
+		ArrayList<String> questions = excelReader.readQuestions("./questions-data/database.xlsx");
+		
+		
+		
 		SpringApplication.run(HumanBodyTestApplication.class, args);
 	}
 
